28a977192e75546dc330812f51481f4d2958e866
Radek Stepan <dev@radekstepan.com>
2016-04-01 08:11:10 -0400

    bump data

--- src/js/components/Chart.jsx
+++ src/js/components/Chart.jsx
@@ -223,8 +223,8 @@ export default React.createClass({
       let tX = width + 10, tY = y(v.s.lastM);
 
       if (k == 'bernie-sanders') tY -= 0;
-      if (k == 'ted-cruz') tY -= 4;
-      if (k == 'john-kasich') tY += 9;
+      if (k == 'ted-cruz') tY += 0;
+      if (k == 'john-kasich') tY += 12;
 
       // The name.
       c.append("text")
