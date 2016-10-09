1dc0291f66d972038ddc1403559ba89712b7c7be
Radek Stepan <dev@radekstepan.com>
2016-10-09 11:39:25 -0400

    data bump

--- data/odds/index.js
+++ data/odds/index.js
@@ -1,3 +1,2 @@
-exports['bernie-sanders'] = require('./bernie-sanders.json');
 exports['donald-trump'] = require('./donald-trump.json');
 exports['hillary-clinton'] = require('./hillary-clinton.json');
